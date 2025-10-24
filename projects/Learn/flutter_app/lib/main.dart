d74bba1f7efa1423e216e96a4d68d6f20a0a25e9
brishibhatia <brishi11128@gmail.com>
2025-10-24 22:54:47 +0530

    puh

--- projects/Learn/flutter_app/lib/main.dart
+++ projects/Learn/flutter_app/lib/main.dart
@@ -18,7 +18,62 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: Scaffold(
-        appBar: AppBar(title: Center(child: Text("data"))),
+        appBar: AppBar(
+          backgroundColor: Colors.amberAccent,
+          title: Text("Flutter Mapp"),
+          centerTitle: true,
+          // leading: Icon(Icons.back_hand_rounded),
+          actions: [
+            Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.only(right: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.red,
+              ),
+              child: Icon(Icons.arrow_back),
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              DrawerHeader(child: Text("App Drawer")),
+              Container(
+                // padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+
+                child: ListTile(
+                  title: Text("Login"),
+                  leading: Icon(Icons.login),
+                  trailing: Icon(Icons.back_hand),
+                  tileColor: Colors.amberAccent,
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+              icon: Icon(Icons.contact_emergency),
+              label: 'Contact',
+            ),
+            NavigationDestination(icon: Icon(Icons.person), label: 'profile'),
+          ],
+          onDestinationSelected: (int value) {
+            print(value);
+          },
+          selectedIndex: 1,
+        ),
       ),
     );
   }
