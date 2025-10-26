46a831f35d5dcd1b0f0272dc7cd8f6909b0a92ef
brishibhatia <brishi11128@gmail.com>
2025-10-26 22:08:39 +0530

    learning flutter

--- projects/Learn/flutter_app/lib/main.dart
+++ projects/Learn/flutter_app/lib/main.dart
@@ -1,79 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/navbar_widget.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+String? title = "Flutter Mapp";
+int selectedIndex = 0;
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.yellow,
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
       ),
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
-          title: Text("Flutter Mapp"),
-          centerTitle: true,
-          // leading: Icon(Icons.back_hand_rounded),
-          actions: [
-            Container(
-              width: 30,
-              height: 30,
-              margin: EdgeInsets.only(right: 15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.red,
-              ),
-              child: Icon(Icons.arrow_back),
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: Column(
-            children: [
-              DrawerHeader(child: Text("App Drawer")),
-              Container(
-                // padding: const EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-
-                child: ListTile(
-                  title: Text("Login"),
-                  leading: Icon(Icons.login),
-                  trailing: Icon(Icons.back_hand),
-                  tileColor: Colors.amberAccent,
-                ),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.add),
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(
-              icon: Icon(Icons.contact_emergency),
-              label: 'Contact',
-            ),
-            NavigationDestination(icon: Icon(Icons.person), label: 'profile'),
-          ],
-          onDestinationSelected: (int value) {
-            print(value);
-          },
-          selectedIndex: 1,
-        ),
+        appBar: AppBar(title: Text(title!), centerTitle: true),
+        // body: selectedIndex == 0
+        //     ? Center(child: Text("data"))
+        //     : Center(child: Text("2")),
+        bottomNavigationBar: NavbarWidget(),
       ),
     );
   }
