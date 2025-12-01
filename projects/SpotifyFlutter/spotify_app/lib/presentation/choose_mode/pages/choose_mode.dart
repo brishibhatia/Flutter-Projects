94469c5605de933f7eff88b21dd2f557543abbd2
Rishi Bhatia <brishi11128@gmail.com>
2025-12-01 22:02:00 +0530

    splashscreenone

---
+++ projects/SpotifyFlutter/spotify_app/lib/presentation/choose_mode/pages/choose_mode.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../core/config/assets/app_vectors.dart';
+
+class ChooseMode extends StatefulWidget {
+  const ChooseMode({super.key});
+
+  @override
+  State<ChooseMode> createState() => _ChooseModeState();
+}
+
+class _ChooseModeState extends State<ChooseMode> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage('assets/images/choose_mode_bg.png'),
+              ),
+            ),
+          ),
+          Container(color: Colors.black.withOpacity(0.15)),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- projects/SpotifyFlutter/spotify_app/lib/presentation/intro/pages/get_started.dart
+++ projects/SpotifyFlutter/spotify_app/lib/presentation/intro/pages/get_started.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_app/common/widgets/basic_app_button.dart';
 import 'package:spotify_app/core/config/assets/app_images.dart';
 import 'package:spotify_app/core/config/assets/app_vectors.dart';
+import 'package:spotify_app/core/config/theme/app_colors.dart';
+import 'package:spotify_app/presentation/choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -11,30 +14,62 @@ class GetStartedPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(
-            child: Image.asset(AppImages.introImage, fit: BoxFit.fill),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: SvgPicture.asset(AppVectors.logo),
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introImage),
+                fit: BoxFit.fill,
               ),
-
-              Spacer(),
-              Text(
-                "Enjoy Listening to Music",
-                style: TextStyle(
-                  fontFamily: 'assets/fonts/Satoshi-Bold.ttf',
-                  fontSize: 25,
-                  color: Color(0xffDADADA),
-                ),
-              ),
-            ],
+            ),
           ),
           Container(color: Colors.black.withOpacity(0.15)),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+
+                Spacer(),
+                Text(
+                  "Enjoy Listening to Music",
+                  style: TextStyle(
+                    fontFamily: 'assets/fonts/Satoshi-Bold.ttf',
+                    fontSize: 25,
+                    color: Color(0xffDADADA),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  reprehenderit optio amet ab temporibus asperiores quasi ducimus voluptates voluptas?",
+                  style: TextStyle(
+                    fontFamily: 'assets/fonts/Satoshi-Regular.ttf',
+                    color: AppColors.grey,
+                    fontSize: 17,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ChooseMode();
+                        },
+                      ),
+                    );
+                  },
+                  title: 'Get Started',
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
