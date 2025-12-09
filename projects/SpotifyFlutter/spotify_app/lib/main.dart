a5b47199d9ac3ead436bfc43ba77ca453709ca24
Rishi Bhatia <brishi11128@gmail.com>
2025-12-09 07:17:11 +0530

    good

--- projects/SpotifyFlutter/spotify_app/lib/main.dart
+++ projects/SpotifyFlutter/spotify_app/lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:spotify_app/core/config/theme/app_theme.dart';
 import 'package:spotify_app/presentation/splash/pages/splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(const MyApp());
 }
+
+Future getTemporaryDirectory() async {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
