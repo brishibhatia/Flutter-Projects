d8596913e025caeb52bfa2da6afe2b7c612398fe
Rishi Bhatia <brishi11128@gmail.com>
2025-12-10 06:59:17 +0530

    good

--- projects/SpotifyFlutter/spotify_app/lib/main.dart
+++ projects/SpotifyFlutter/spotify_app/lib/main.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/config/theme/app_theme.dart';
+import 'package:spotify_app/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify_app/presentation/splash/pages/splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HydratedBloc.storage = await HydratedStorage.build(
+
+  final storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
-        ? HydratedStorageDirectory.web
-        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
   );
+
+  HydratedBloc.storage = storage;
+
   runApp(const MyApp());
 }
-
-Future getTemporaryDirectory() async {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: SplashPage(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => ThemeCubit())],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (_, themeState) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeState,
+            home: SplashPage(),
+          );
+        },
+      ),
     );
   }
 }
